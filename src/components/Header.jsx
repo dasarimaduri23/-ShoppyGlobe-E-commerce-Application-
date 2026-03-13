38780af407571bc5c99c65658b1775c607d9e158
dasarimaduri23 <dasarimaduri23@gmail.com>
2026-03-13 15:23:08 +0530

    added cart length functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import logo from "../assets/logo.png";
+import { useSelector } from 'react-redux';
 function Header() {
     
-
+    const cartItems = useSelector(state => state.cartSlice.cartItems);
     return (
         <header className="fixed top-0 shadow-lg rounded-b-[30px] bg-white z-10 w-full flex items-center justify-between px-5 min-[500px]:px-10 h-20">
             <figure>
@@ -18,8 +19,8 @@ function Header() {
                     <Link to="/"><li className="font-semibold text-lg">Home</li></Link>
                     <Link to="/cart">
                         <li className="relative font-semibold">
-                            <FontAwesomeIcon className='mt-1 text-lg text-sky-900' icon={faCartShopping} />
-                            <span className='absolute -top-2 -right-3 text-lg'></span>
+                            <FontAwesomeIcon className='mt-1 text-lg text-sky-500' icon={faCartShopping} />
+                            <span className='absolute -top-2 -right-3 text-lg text-sky-500 '>{cartItems.length ? cartItems.length : 0}</span>
                         </li>
                     </Link>
                 </ul>
