83a7f27cd744a617070c213f06d1a3ec4bf2438b
dasarimaduri23 <dasarimaduri23@gmail.com>
2026-03-13 11:17:29 +0530

    added reducers to the productsStore

--- src/utils/store/productsStore.jsx
+++ src/utils/store/productsStore.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import productsSlice from "../store/slices/productsSlice";
+import cartSlice from "../store/slices/cartSlice";
 
 const productsStore = configureStore({
     reducer: {
-       
+        productsSlice: productsSlice,
+        cartSlice: cartSlice
     }
 });
 
